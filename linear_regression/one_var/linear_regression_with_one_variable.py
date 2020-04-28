7faf1eca247e928664d2e58d10eb678097acc9a0
Jasmeet Singh <jasmeet0915@gmail.com>
2020-04-28 14:51:50 +0530

    plotting funtion added

--- linear_regression/one_var/linear_regression_with_one_variable.py
+++ linear_regression/one_var/linear_regression_with_one_variable.py
@@ -2,6 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+dataset = pd.read_csv("/home/singh/PycharmProjects/MachineLearning_from_Scratch/Datasets/linear_regression"+
+                      "/one_var/train.csv")
+
+print("Shape of dataset: " + str(dataset.shape))
+print("\ndataset head: \n" + str(dataset.head()))
+
+data_x = dataset['x'][:50]
+data_y = dataset['y'][:50]
+
+data_x = data_x.to_numpy()
+data_x = data_x.reshape(len(data_x), 1)
+
 
 # hypothesis function of the form y = c1 + c2*x
 def h(c1, c2, xh):
@@ -15,47 +27,35 @@ def h(c1, c2, xh):
     return yh
 
 
-# cost function of the form of squared mean error function
-def j(p1, p2, m, x, y):
-    sum = 0
+''''# cost function of the form of squared mean error function
+def j(pred, x, y):
+    # use different params to calculate different h_y and send them as preds to j() for plotting
+    # this way it will be easier for j to calculate and will be faster as h() will be caculated with matrix multiplication fastly
 
-    # loop to calculate the sum of squared errors
-    for i in range(0, m):
-        sum = sum + (h(p1, p2, x[i]) - y[i])**2
-
-    # calculating the mean of squared errors
-    sq_mean_err = sum/m
-    sq_mean_err = sq_mean_err/2
-    return sq_mean_err
+'''
 
 
-dataset = pd.read_csv("/home/singh/PycharmProjects/MachineLearning_from_Scratch/Datasets/linear_regression"+
-                      "/one_var/train.csv")
+def plot_all(para1, para2, x_data, y_data):
+    y_pred = h(para1, para2, x_data)
 
-print("Shape of dataset: " + str(dataset.shape))
-print("\ndataset head: \n" + str(dataset.head()))
+    plt.style.use("seaborn")
+    fig, (ax_h, ax_train) = plt.subplots(2, 1, sharex=True)
 
-data_x = dataset['x'][:50]
-data_y = dataset['y'][:50]
+    # plotting the scatter plot of training data
+    ax_train.set_title("training data")
+    ax_train.set_ylabel("random nums generated")
+    ax_train.set_xlabel("data x")
+    ax_train.scatter(x_data, y_data)
 
-data_x = data_x.to_numpy()
-data_x = data_x.reshape(len(data_x), 1)
-h_y = h(0, 1, data_x)
+    # plotting the hypothesis funtion
+    ax_h.set_title("h(x) plot")
+    ax_h.set_ylabel("h(x)-predictions")
+    ax_h.plot(x_data, y_pred)
 
-plt.style.use("seaborn")
-fig, (ax_train, ax_h) = plt.subplots(2, 1, sharex=True)
+    plt.show()
 
-ax_train.set_title("Training Data Plot")
-ax_train.scatter(data_x, data_y)
-ax_train.set_ylabel("Random nums generated")
 
-ax_h.set_title("Hypothesis Function Plot")
-ax_h.set_xlabel("x_numbers")
-ax_h.set_ylabel("predictions by h(x)")
-ax_h.plot(data_x, h_y, 'm')
-
-plt.show()
-
+plot_all(0, 1, data_x, data_y)
 
 
 
