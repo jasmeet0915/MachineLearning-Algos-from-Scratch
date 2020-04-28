aa73f74f120dfce608df11a34827e139d86cb542
Jasmeet Singh <jasmeet0915@gmail.com>
2020-04-28 15:04:39 +0530

    train.py added

---
+++ linear_regression/one_var/train.py
@@ -0,0 +1,6 @@
+# File for calculating the best parameters for the hypothesis function, h(x),
+# which will be used for predictions. Gradient descent will be used for that on the
+# mean squared cost function'''
+
+
+
